b5f2d35e00750303df50668a380eee4426eb098e
apolinario <apolinariocruz.f@gmail.com>
2025-04-17 21:16:50 -0300

    desenvolvendo react

--- senai-gpt-web-react/src/pages/login/index.jsx
+++ senai-gpt-web-react/src/pages/login/index.jsx
@@ -21,49 +21,87 @@ function Login() {
         email: email,
         password: password
       })
-    
+
     });
 
     console.log(response)
-
-}
-
-
-return (
-  <>
-
-    <header></header>
-
-    <main className="page-conteiner">
-      <div className="robo-image">
-
-        <img src="../ASSETS/Rectangle 1.png" alt="" />
-
-      </div>
-
-      <div className="login-conteiner">
-
-        <img className="logo" src={logo} alt="logo seneiGPT" />
+    if (response.ok == true) {
+      alert("login realizado com sucesso");
+      console.log(response)
+      let json = await response.json()
+      let token = json.accessToken;
+      console.log("token:" + token);
+      localStorage.setItem("meuToken", token);
+      
+      //function setcookie(nome, value, days) {
+        //const date = new date();
+        //date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        //const expires = "expires=" + date.toUTCString();
+        //document.cookie = `${nome}=${value};${expires};path=/`;
 
 
+     // }
+     // setcookie("meuToken", token, 7);
+     window.location.href = "/chat";
 
-        <h1 id="meutitulo" className="titulo">login</h1>
+    } else {
+
+      if (response.status == 401) {
+        alert("credeciais nao encontrada");
+      } else {
+        alert("erro inesperado")
+      }
+    }
+
+  }
 
 
-        <input className="inpt" valeu={email} onChange={event => setEmail(event.target.valeu)} type="email" placeholder=" insira o e-mail" />
+  return (
+    <>
 
-        <input className="inpt" valeu={password} onChange={event => setpassword(event.target.value)} type="password" placeholder="senha" />
+      <header></header>
 
-        <button className="btn" onClick={() => onLoginClick()}>enviar</button>
+      <main className="page-conteiner">
+        <div className="robo-image">
 
-      </div>
+          <img src="../ASSETS/Rectangle 1.png" alt="" />
 
-    </main>
+        </div>
+
+        <div className="login-conteiner">
+
+          <img className="logo" src={logo} alt="logo seneiGPT" />
 
 
 
-  </>
-)
+          <h1 id="meutitulo" className="titulo">login</h1>
+
+
+          <input className="inpt" valeu={email} onChange={event => setEmail(event.target.value)} type="email" placeholder=" insira o e-mail" />
+
+          <input className="inpt" valeu={password} onChange={event => setpassword(event.target.value)} type="password" placeholder="senha" />
+
+          <button className="btn" onClick={() => onLoginClick()}>enviar</button>
+
+        </div>
+
+      </main>
+
+
+
+    </>
+  )
 }
 
 export default Login;
+
+// rest api
+// get serve para trazer uma lista de infomações
+// post serve para envir as inforçãoes
+// put serve pata fazer atualização
+// delete e ultilizado para remover
+
+//  erros
+// 200 a 299 tudo certo
+// 400 a 499 erro do front
+// 500 a 599 erro do back
